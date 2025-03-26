c3a65846150b5ce6cccce2bb8ec9fccc4b9dd114
GF63-1434 <mwahyufikri31@gmail.com>
2025-03-26 14:09:19 +0700

    berhasil menampilkan semua data

--- controller.js
+++ controller.js
@@ -4,5 +4,20 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi REST API ku berjalan!",res);
+    response.ok("Aplikasi REST API ku berjalan!",res)
 };
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa',function(error,rows,fields){
+        if(error){
+            connection.log(error);
+
+        }
+        else{
+            response.ok(rows,res)
+        }
+
+    });
+}
+    
