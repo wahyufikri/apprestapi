e6ccc4e147411106abbcc441ecffd6edcc95d3d7
GF63-1434 <mwahyufikri31@gmail.com>
2025-03-26 14:26:54 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -11,7 +11,7 @@ exports.index = function(req,res){
 exports.tampilsemuamahasiswa = function(req,res){
     connection.query('SELECT * FROM mahasiswa',function(error,rows,fields){
         if(error){
-            connection.log(error);
+            console.log(error);
 
         }
         else{
@@ -19,5 +19,24 @@ exports.tampilsemuamahasiswa = function(req,res){
         }
 
     });
-}
+
+};
+
+
+//menampilkan data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?',[id],
+    function(error,rows,fields){
+        if(error){
+            console.log(error);
+
+        }
+        else{
+            response.ok(rows,res)
+        }
+
+    });
+
+};
     
