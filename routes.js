4d4dd48a08d1b57a716777a2b52e1d4acb69454a
GF63-1434 <mwahyufikri31@gmail.com>
2025-04-10 11:47:23 +0700

    hapus data

--- routes.js
+++ routes.js
@@ -15,4 +15,7 @@ module.exports = function(app){
     app.route('/ubah')
         .put(jsonku.ubahMahasiswa);
 
+    app.route('/hapus')
+        .delete(jsonku.hapusMahasiswa);
+
 }                                                                                                                                                                           
